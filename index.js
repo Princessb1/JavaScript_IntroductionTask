54d1d71532ff1222cca0183fa5da506aef1e7337
Princessb1 <blessing.r.garba@gmail.com>
2022-06-21 23:23:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let myName = "Garba Blessing Regina";
+let myHeight = "1.60m";
+let myCountry = "Nigeria";
+
+document.getElementById("myName").innerHTML = "My Name is: " + myName;
+document.getElementById("myHeight").innerHTML = "My Height is: " + myHeight;
+document.getElementById("myCountry").innerHTML = "My Country is: " + myCountry;
